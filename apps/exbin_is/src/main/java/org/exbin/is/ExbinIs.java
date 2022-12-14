f539a166adcaa0221d7b6d82930840b25a79dab1
hajdam <hajdam@users.sf.net>
2022-12-14 23:31:30 +0100

    Headers cleanup

--- apps/exbin_is/src/main/java/org/exbin/is/ExbinIs.java
+++ apps/exbin_is/src/main/java/org/exbin/is/ExbinIs.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -48,8 +48,7 @@ import org.exbin.framework.preferences.PreferencesWrapper;
 /**
  * The main class of the ExbinIs application.
  *
- * @version 0.2.0 2016/08/18
- * @author ExBin Project (http://exbin.org)
+ * @author ExBin Project (https://exbin.org)
  */
 @ParametersAreNonnullByDefault
 public class ExbinIs {
